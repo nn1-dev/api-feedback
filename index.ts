e0c0f4df4260561f4f4b588468f603f9b7618ead
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2024-06-26 19:12:29 +0100

    Add auth

--- index.ts
+++ index.ts
@@ -12,19 +12,19 @@ const HANDLER_MAPPER = {
 };
 
 Deno.serve(async (request) => {
-  // const auth = request.headers.get("Authorization");
+  const auth = request.headers.get("Authorization");
 
-  // if (auth !== `Bearer ${Deno.env.get("GH_MERGE_API_KEY")}`) {
-  //   return Response.json(
-  //     {
-  //       status: "error",
-  //       statusCode: 401,
-  //       data: null,
-  //       error: "Nice try 👍",
-  //     },
-  //     { status: 401 },
-  //   );
-  // }
+  if (auth !== `Bearer ${Deno.env.get("NN1_DEV_API_KEY")}`) {
+    return Response.json(
+      {
+        status: "error",
+        statusCode: 401,
+        data: null,
+        error: "Nice try 👍",
+      },
+      { status: 401 },
+    );
+  }
 
   return await HANDLER_MAPPER[request.method as keyof typeof HANDLER_MAPPER](
     request,
