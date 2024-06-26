fd6f890e1ba0fd96cfc1116326efe4167be4fade
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2024-06-26 18:46:31 +0100

    Add post handler

--- handlerPost.ts
+++ handlerPost.ts
@@ -1,12 +1,9 @@
-// import { PREFIX } from "./constants.ts";
+import { PREFIX } from "./constants.ts";
 
 const handlerPost = async (request: Request, kv: Deno.Kv) => {
   const body: Entry = await request.json();
 
-  // await kv.set(
-  //   [PREFIX, body.owner, body.repo, body.pull_number],
-  //   body.schedule,
-  // );
+  await kv.set([PREFIX, Date.now()], body);
 
   return Response.json(
     {
--- types.d.ts
+++ types.d.ts
@@ -1,6 +1,7 @@
 interface Entry {
-  owner: string;
-  repo: string;
-  pull_number: number;
-  schedule: string;
+  name: string;
+  stack: string;
+  who: string;
+  interval: string;
+  feedback: string;
 }
