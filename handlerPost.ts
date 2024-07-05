a42640b5f2250c78199b89a19d22e2b479873524
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2024-07-05 21:01:13 +0100

    Test email sending

--- handlerPost.ts
+++ handlerPost.ts
@@ -1,4 +1,7 @@
+import { Resend } from "npm:resend";
 import { PREFIX } from "./constants.ts";
+
+const resend = new Resend(Deno.env.get("RESEND_API_KEY"));
 
 const handlerPost = async (request: Request, kv: Deno.Kv) => {
   const body: Entry = await request.json();
@@ -8,6 +11,25 @@ const handlerPost = async (request: Request, kv: Deno.Kv) => {
   };
 
   await kv.set([PREFIX, crypto.randomUUID()], data);
+
+  const { error } = await resend.emails.send({
+    from: "NN1 Dev Club <club@nn1.dev>",
+    to: ["club@nn1.dev"],
+    subject: "Feedback",
+    html: "<strong>it works!</strong>",
+  });
+
+  if (error) {
+    return Response.json(
+      {
+        status: "error",
+        statusCode: 400,
+        data: null,
+        error,
+      },
+      { status: 400 },
+    );
+  }
 
   return Response.json(
     {
